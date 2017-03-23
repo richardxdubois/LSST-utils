1119f27d62e28cdd47102106996cf6c75174ccc9
richardxdubois <richard.x.dubois@gmail.com>
2017-03-22 19:01:39 -0700

    add get_steps_schema, tabulateRaftGains

---
+++ python/get_steps_schema.py
@@ -0,0 +1,32 @@
+from getResults import getResults
+
+import argparse
+
+
+## Command line arguments
+parser = argparse.ArgumentParser(description='Find archived data in the LSST  data Catalog. These include CCD test stand and vendor data files.')
+
+##   The following are 'convenience options' which could also be specified in the filter string
+parser.add_argument('-r', '--run', default=None,help="(raft run number (default=%(default)s)")
+args = parser.parse_args()
+
+print 'Discover step and schema names for run ', args.run
+
+eT = getResults( dbConnectFile='devdb_connect.txt')
+
+eT.connectDB()
+
+returnData  = eT.getRunResults(args.run)
+    
+for step in returnData['steps']:
+        stepDict = returnData['steps'][step]
+        print '\n Step ', step, '\n'
+
+        for schemaList in stepDict:
+            print schemaList
+
+
+
+
+    
+
---
+++ python/tabulateRaftGains.py
@@ -0,0 +1,62 @@
+from getResults import getResults
+from exploreRaft import exploreRaft
+
+import argparse
+
+
+## Command line arguments
+parser = argparse.ArgumentParser(description='Find archived data in the LSST  data Catalog. These include CCD test stand and vendor data files.')
+
+##   The following are 'convenience options' which could also be specified in the filter string
+parser.add_argument('-r','--raftID', default=None,help="(metadata) Raft ID (default=%(default)s)")
+parser.add_argument('--run', default=None,help="(raft run number (default=%(default)s)")
+parser.add_argument('-s','--schema', default=None,help="(metadata) schema (default=%(default)s)")
+parser.add_argument('-X','--XtraOpts',default=None,help="any extra 'datacat find' options (default=%(default)s)")
+args = parser.parse_args()
+
+
+raft = args.raftID
+schema = args.schema
+
+print 'Searching ', raft, ' for ', schema
+
+eR = exploreRaft(db='Dev')
+eT = getResults( dbConnectFile='devdb_connect.txt')
+
+eT.connectDB()
+
+ccd_list = eR.raftContents(raft)
+
+
+for row in ccd_list:
+    ccd = str(row[0])
+    print ccd
+    print row
+    print 'Amplifier           TS8 Fe55 Gain         TS8 PTC Gain'
+    
+    test_table = {}
+    returnData  = eT.getRunResults(args.run,itemFilter=('sensor_id', ccd))
+    
+    for step in returnData['steps']:
+        stepDict = returnData['steps'][step]
+
+        for schemaList in stepDict:
+            if schemaList == 'fe55_raft_analysis':
+                for d in stepDict[schemaList]:
+                    if d['schemaInstance'] == 0: continue
+                    test_table[d['amp']] = [d['gain'], -99.]
+            if schemaList == 'ptc_raft':
+                for d in stepDict[schemaList]:
+                    if d['schemaInstance'] == 0: continue
+                    test_table[d['amp']][1] = d['ptc_gain']
+
+    
+    for amp in test_table:
+        print "%8i              %6.2f              %6.2f" % (amp, test_table[amp][0], test_table[amp][1])
+        
+
+
+
+
+    
+
