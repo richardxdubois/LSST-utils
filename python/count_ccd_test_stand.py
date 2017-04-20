6fa1fff42102fdfe7bf9cfcf9e73ab69223b37b9
richardxdubois <richard.x.dubois@gmail.com>
2017-04-20 15:45:31 -0700

    add count_ccd_test_stand.py - counts up sensors through TS3

---
+++ python/count_ccd_test_stand.py
@@ -0,0 +1,44 @@
+from getResults import getResults
+from exploreRaft import exploreRaft
+from  eTraveler.clientAPI.connection import Connection
+
+import argparse
+
+
+## Command line arguments
+parser = argparse.ArgumentParser(description='Find archived data in the LSST  data Catalog. These include CCD test stand and vendor data files.')
+
+##   The following are 'convenience options' which could also be specified in the filter string
+parser.add_argument('-t','--htype', default=None,help="hardware type (default=%(default)s)")
+parser.add_argument('-d','--db',default='Prod',help="eT database (default=%(default)s)")
+parser.add_argument('-e','--eTserver',default='Prod',help="eTraveler server (default=%(default)s)")
+args = parser.parse_args()
+
+
+if args.eTserver == 'Prod': pS = True
+else: pS = False
+connect = Connection(operator='richard', db=args.db, exp='LSST-CAMERA', prodServer=pS)
+
+   
+returnData  = connect.getResultsJH(htype=args.htype, stepName = 'vendorIngest', travelerName='SR-RCV-01')
+
+ccd_list_all = []
+ccd_list = []
+for ccd in returnData:
+    ccd_list_all.append(ccd)
+    
+    try:
+        returnDataTS3  = connect.getResultsJH(htype=args.htype, stepName = 'fe55_acq', travelerName='SR-EOT-1', experimentSN=ccd)
+        ccd_list.append(ccd)
+    except:
+        pass
+
+print len(ccd_list_all), len(ccd_list), '\n \n', ccd_list
+    
+        
+
+
+
+
+    
+
