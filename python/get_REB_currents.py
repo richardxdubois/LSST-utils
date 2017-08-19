96d0c1f819c4d64a0df13f899ad2659873b664b3
richardxdubois <richard.x.dubois@gmail.com>
2017-08-19 14:50:26 -0700

    rewrite of get_reb_currents by Joanne to use api for manual entries

--- python/get_REB_currents.py
+++ python/get_REB_currents.py
@@ -1,72 +1,94 @@
-import sqlalchemy
+##import sqlalchemy
 import argparse
-
+from eTraveler.clientAPI.connection import Connection
 
 ## Command line arguments
-parser = argparse.ArgumentParser(description='Find archived data in the LSST  data Catalog. These include CCD test stand and vendor data files.')
+parser = argparse.ArgumentParser(description='Extract manual operator entries for re-recorded currents for REBs')
 
-##   The following are 'convenience options' which could also be specified in the filter string
-parser.add_argument('-s','--stepName', default="SR-REB-VER-01_step6",help="step name in traveler (default=%(default)s)")
 parser.add_argument('--minREB', default=0,help="min REB number (default=%(default)s)")
 parser.add_argument('--maxREB', default=1000,help="max REB number (default=%(default)s)")
-parser.add_argument('--db', default='db_connect.txt',help="db connect file (default=%(default)s)")
+parser.add_argument('-d', '--db', default='Prod', help="eT database (default=%(default)s)")
+parser.add_argument('-e', '--eTserver', default='Dev', help="eTraveler server (default=%(default)s)")
+parser.add_argument('--appSuffix', '--appSuffix', default='jrb',
+                    help="eTraveler Dev server instance (default=%(default)s); ignored if --etServer=Prod")
+
 args = parser.parse_args()
 
-kwds = {}
-with open(args.db) as f:
-    for line in f:
-        (key, val) = line.split()
-        kwds[key] = val
-
-
-# extract manually submited currents from REB testing
-
-# Create a new mysql connection object.
-db_url = sqlalchemy.engine.url.URL('mysql+mysqldb', **kwds)
-engine = sqlalchemy.create_engine(db_url)
-mysql_connection = engine.raw_connection()
-
-print 'connected to host' , kwds['host']
-
-cursor = mysql_connection.cursor()
-
 # LCA-13574 is the REB.
+rebType='LCA-13574'
+travelerName='SR-REB-VER-01'
+stepNames=['SR-REB-VER-01_step6', 'SR-REB-VER-01_step5']
 
-sql = "select hw.lsstId, res.activityId, act.rootActivityId, ip.label, res.value from StringResultManual res join Activity act on res.activityId=act.id JOIN Hardware hw ON act.hardwareId=hw.id join Process pr on act.processId=pr.id join InputPattern ip on  res.inputPatternId=ip.id where pr.name='" + args.stepName + "' order by res.activityId asc"
+# extract manually submitted currents from REB testing
 
-result = engine.execute(sql)
+# Create a new eTraveler API connection 
+if args.eTserver == 'Prod':
+    connect = Connection(operator='richard', db=args.db, exp='LSST-CAMERA',
+                         prodServer=True)
+else:
+    pS = False
+    connect = Connection(operator='richard', db=args.db, exp='LSST-CAMERA',
+                         prodServer=False, appSuffix='-' + args.appSuffix)
+
 table_dict = {}
 label_dict = {}
 new_reb = ''
 
 print 'min, max REB ', args.minREB, args.maxREB
+print '\n\n'
 
-for res in result:
-    reb = res['lsstId']
-    reb_num = int(reb.split("-")[-1])
-    if reb_num < int(args.minREB) or reb_num > int(args.maxREB): continue
+for stepName in stepNames:
+    try:
+        rsp = connect.getManualResultsStep(travelerName=travelerName,
+                                           stepName=stepName,
+                                           hardwareType=rebType)
+    except Exception, msg:
+        print msg
+        raise
+
+    for cmp in rsp:
+        cmpdata = rsp[cmp]
+        reb = cmpdata['experimentSN']
+        reb_num = int(reb.split("-")[-1])
+        if reb_num < int(args.minREB) or reb_num > int(args.maxREB): continue
         
-    if reb not in table_dict: table_dict[reb]={}
-    value = res['value']
-    label = res['label'].split('at')[-1]
-    if '<' in label: label = label.split('<')[0]
-    table_dict[reb][label] = value
-    
-for reb in table_dict:
-    label_title = '                '
-    reb_list = table_dict[reb]
-    for label in reb_list:
-        label_title += label + ' '
-    break
-print label_title
+        if reb not in table_dict: table_dict[reb]={}
+
+        stepdata = cmpdata['steps'][stepName]
+        for entryname in stepdata:
+            entrydict = stepdata[entryname]
+            if entrydict['isOptional'] == 1: continue
+            if entrydict['datatype'] != 'string': continue
+            parts = entryname.split('_current_at_')
+            if len(parts) != 2: continue
+            label=parts[1]
+            value=entrydict['value']
+            table_dict[reb][label] = value
+
+rebNameWidth = 13
+aReb = list(table_dict)[0]
+reb_dict = table_dict[aReb]
+
+fieldWidth = max(len(k) for k in reb_dict) + 2
+formatTitle = 'REB          '
+formatValues = '{:' + str(rebNameWidth) + '}'
+ix = 0
+label_list = []
+for k in reb_dict:
+    label_list.append(k)
+    formatTitle += ('{d[' + str(ix) + ']:>' + str(fieldWidth) + '}')
+    formatValues += '{valueDict[' + k + ']:>' + str(fieldWidth) + '}'
+    ix += 1
+
+#print 'formatTitle string is:  ', formatTitle
+#print 'formatValues string is: ', formatValues
+#print '\n\n\n'
+print formatTitle.format(d=label_list)
 
 for reb in sorted(table_dict):
-    value_title = '                  '
-    print reb
-    reb_list = table_dict[reb]
-    for label in reb_list:
-        value_title += reb_list[label] + '          '
-    print value_title
+    valueString = formatValues.format(reb, valueDict=table_dict[reb])
+    print valueString
+
 
     
 
