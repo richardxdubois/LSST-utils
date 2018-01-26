e53293f2085fb64a35b11ae41adb2c0d1336b957
richardxdubois <richard.x.dubois@gmail.com>
2018-01-26 11:00:33 -0800

    allow selecting image file seq number

--- python/vis_amplifier.py
+++ python/vis_amplifier.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 class vis_amplifier():
 
     def __init__(self, testName=None, CCDType=None, sensorId=None, XtraOpts = 'IMGTYPE=="FLAT"', run= None,
-                 amp=None, mirror='BNL-Prod'):
+                 amp=None, mirror='BNL-Prod', imageNum=1):
 
        self.testName = testName
        self.CCDType = CCDType
@@ -17,6 +17,7 @@ class vis_amplifier():
        self.amp = amp
        self.XtraOpts = XtraOpts
        self.mirror = mirror
+       self.imageNum = imageNum
 
     def make_plt(self):
         
@@ -29,7 +30,7 @@ class vis_amplifier():
        # see LSSTTD-437 for treatment of bias using bias files, not (only) the overscan region.
 
 
-       file = files[0]
+       file = files[self.imageNum]
        print file
 #       file = '/Users/richard/LSST/Data/RTM-005/E2V-CCD250-220_sflat_500_flat_H011_20171213042022.fits'
 #       file = '/Users/richard/LSST/Data/RTM-005/E2V-CCD250-252_sflat_500_flat_H001_20170523024234.fits'
@@ -62,6 +63,7 @@ if __name__ == "__main__":
     parser.add_argument('-a','--amplifier', default=1,help="amplifier number (default=%(default)s)")
     parser.add_argument('-t','--type', default='dark',help="file kind (default=%(default)s)")
     parser.add_argument('-r','--run', default=None,help="run number (default=%(default)s)")
+    parser.add_argument('-i','--imageNum', default=0,help="run number (default=%(default)s)")
     parser.add_argument('-m','--mirror', default='BNL-Prod',help="DC mirror ")
     parser.add_argument('-o','--output', default='amplifier.pdf',help="output pdf file ")
     args = parser.parse_args()
@@ -69,7 +71,7 @@ if __name__ == "__main__":
     
 
     vA = vis_amplifier(testName=args.type, sensorId=args.sensorID, run=args.run, amp=args.amplifier,
-                       mirror=args.mirror)
+                       mirror=args.mirror, imageNum=int(args.imageNum))
 
     bias_sub = vA.make_plt()
 
