6abf710aae5ac70e1e567fa5d5c33a5793bafce1
Richard Dubois <dubois@stanford.edu>
2019-12-30 12:19:47 -0800

    first commit - test file for Bo

---
+++ python/t_user_hook.py
@@ -0,0 +1,42 @@
+import numpy as np
+from collections import OrderedDict
+import random
+
+
+def hook(run=None, mode=None, raft=None, ccd=None, test_cache=None):
+    """
+    User hook for test quantity
+    :param run: run number
+    :return: list of user-supplied quantities to be included in the heat map
+    """
+# SW0 and SW1 only use 0-7 for resukts. Arrange the indexes to overwrite the back half of
+# SW0 with the front half of SW1
+
+    s = {"SG0":0, "SG1":16, "SW0":32, "SW1":40,
+                  "S00":0, "S01":16, "S02":32, "S10":48,
+                  "S11":64, "S12":80, "S20":96, "S21":112, "S22":128}
+    slot_index = OrderedDict(s)
+
+    if raft in ["R00", "R04", "R40", "R44"]:
+        test_len = 48
+        out_list = [-1.] * test_len
+        return
+    else:
+        test_len = 144
+
+    out_list = [-1.]*test_len
+
+    for ccd in slot_index:
+        if "SG" in ccd or "SW" in ccd:   # ignore CR
+            continue
+
+        # using list comprehension + randrange()
+        # to generate random number list
+        res = [random.randrange(1, 50, 1) for i in range(15)]
+
+        amp = 0
+        for val in res:
+            out_list[amp + slot_index[ccd]] = val
+            amp += 1
+
+    return out_list
