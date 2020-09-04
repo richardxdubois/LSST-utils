152bebad46473adb3fb8294466a5079ea9cf1f25
Richard Dubois <richard.x.dubois@gmail.com>
2020-09-04 15:13:58 -0700

    batch submission trials

--- python/batch_ccd_spacing.py
+++ python/batch_ccd_spacing.py
@@ -36,6 +36,7 @@ for combos in cS.file_paths:
                   + " --pickle " + args.pickle + " -c " + combos
     command_args += distort_file + spot_files + out_csv + out_files
     print(command_args)
-    subprocess.run(["bsub", command_args])
+    #subprocess.run(["bsub", command_args])
+    subprocess.run(["bsub", "-W 1 ls -l"])
     if args.single == "yes":
         break
