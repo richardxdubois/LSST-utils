3f75e8aae2b460ae6887dbd7e788f6e9f0547f8a
Richard Dubois <richard.x.dubois@gmail.com>
2020-09-04 14:51:59 -0700

    miniconda

--- bin/batch_ccd_spacing.sh
+++ bin/batch_ccd_spacing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ~/anaconda3/etc/profile.d/conda.sh
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate bokeh_env
 export PYTHONPATH=u/ey/richard/LSST/mixcoatl/python
 python /u/ey/richard/LSST/LSST-utils/python/run_ccd_spacing.py $*
