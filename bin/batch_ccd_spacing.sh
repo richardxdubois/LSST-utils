c9a198474d24076ae829391681831073e25be8ad
Richard Dubois <richard.x.dubois@gmail.com>
2020-09-04 14:53:02 -0700

    pythonpath

--- bin/batch_ccd_spacing.sh
+++ bin/batch_ccd_spacing.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate bokeh_env
-export PYTHONPATH=u/ey/richard/LSST/mixcoatl/python
+export PYTHONPATH=/u/ey/richard/LSST/mixcoatl/python
 python /u/ey/richard/LSST/LSST-utils/python/run_ccd_spacing.py $*
