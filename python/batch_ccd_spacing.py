281c18fc8f3ba119c30b7f8d7d890491999bef72
Richard Dubois <richard.x.dubois@gmail.com>
2020-09-04 14:14:48 -0700

    more args to batch

--- python/batch_ccd_spacing.py
+++ python/batch_ccd_spacing.py
@@ -31,7 +31,8 @@ for combos in cS.file_paths:
     out_csv = " --out_params " + args.out_params + combos + ".csv"
     log_file = args.logs + combos + ".log"
 
-    command_args = "-W 20 -R rhel7 -o " + log_file + " python batch_ccd_spacing.sh --single yes -c " + combos
+    command_args = "-W 20 -R rhel7 -o " + log_file + " bash batch_ccd_spacing.sh --single " \
+                  + args.single + + "--pickle " + args.pickle + " -c " + combos
     command_args += distort_file + spot_files + out_csv + out_files
     print(command_args)
     #subprocess.run(["bsub", command_args])
