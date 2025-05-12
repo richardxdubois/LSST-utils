65d23efbc61582f36eeefb520b258f9f10d5ecae
Richard Dubois <richard.x.dubois@gmail.com>
2025-05-12 14:04:06 -0700

    add more tests, from cti

--- python/get_amp_data_2pkl.py
+++ python/get_amp_data_2pkl.py
@@ -47,11 +47,11 @@ if args.do_calib == "yes":
                 r0_name = r0._detectorName
 
                 for test in inputs[series]:
-                    print("test")
+                    print(test)
                     try:
                         g = getattr(r0, test)
                         amp_data[test] = g
-                        print("added test to amp_data", len(g))
+                        print("added test to amp_data", len(g), g)
                     except AttributeError:  # test not available in this calibration
                         pass
                 break
