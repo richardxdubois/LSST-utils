82f800128e9f352f6d1184f02aaed6b151d0c97e
Richard Dubois <richard.x.dubois@gmail.com>
2025-05-12 14:07:14 -0700

    add more tests, from cti

--- python/get_amp_data_2pkl.py
+++ python/get_amp_data_2pkl.py
@@ -47,18 +47,18 @@ if args.do_calib == "yes":
                 r0_name = r0._detectorName
 
                 for test in inputs[series]:
-                    print(test)
+                    #print(test)
                     try:
                         g = getattr(r0, test)
-                        amp_data[test] = g
-                        print("added test to amp_data", len(g), g)
+                        amp_data[r0_name][test] = g
+                        #print("added test to amp_data", len(g), g)
                     except AttributeError:  # test not available in this calibration
                         pass
-                break
+                #break
 
         except:
             print("Failed to access calibration ", args.ticket_calib)
-        break
+        #break
 
     o = args.ticket_calib + ".npy"
     print("about to write to ", o)
